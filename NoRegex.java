6d157c0e1813b5599f8ea7cdd93cae038ac5c824
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-01-19 12:02:19 +0100

    Add files via upload

---
+++ NoRegex.java
@@ -0,0 +1,36 @@
+package RegexProject;
+
+public class NoRegex {
+    public boolean validateSPZ(String text){
+        if (text==null)
+            return false;
+        text=text.toUpperCase().trim();
+        if (text.length()!=7)
+            return false;
+        for (int i=0;i<=1;i++){
+            if (!Character.isLetter(text.charAt(i)));
+            return false;
+        }
+        for (int i=2;i<=6;i++){
+            if (!Character.isLetterOrDigit(text.charAt(i)));
+            return false;
+        }
+        return true;
+    }
+    public  boolean validateID(String text){
+        if (text==null)
+            return false;
+        text=text.toUpperCase().trim();
+        if (text.length()!=8)
+            return false;
+        for (int i=0;i<=1;i++){
+            if (!Character.isLetter(text.charAt(i)));
+            return false;
+        }
+        for (int i=2;i<=7;i++){
+            if (!Character.isDigit(text.charAt(i)));
+            return false;
+        }
+        return true;
+    }
+}
